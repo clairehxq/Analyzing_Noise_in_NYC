f841b0eeda541593f40585b247a42b0aa6ef1db7
clairehxq <xh895@login-2-1.local>
2017-04-25 00:30:13 -0400

    reduce job done!!!

--- py/counts01.py
+++ py/counts01.py
@@ -4,8 +4,11 @@ import time
 import datetime
 import pyspark
 from operator import add
-#sc = pyspark.SparkContext()
 
+import sys                                                                
+sys.stdout = open('../output/reduce.txt', 'w')  
+
+sc = pyspark.SparkContext()
 
 path_noise ='hdfs:///user/xh895/BDM_Project/311_Noise_Complaints.csv'
 path_comp_all = 'hdfs:////user/xh895/BDM_Project/311_dl_32817.csv'
@@ -13,7 +16,7 @@ path_local = '/Users/macbook/Google Drive/BDM-project/311_dl_32817.csv'
 
 #noise  = sc.textFile(path_noise, 8)
 #all_comp = sc.textFile(path_comp_all, 8)
-all_comp = sc.textFile(path_local, 8)
+all_comp = sc.textFile(path_comp_all, 8)
 
 # refer column names & loc in /output/complaints_column.csv
 def ft_header(row):
@@ -23,7 +26,7 @@ def ft_noise(row):
 	'''
 	this function filters complaint type with Noise
 	'''
-	return row[4][:5] == 'NOISE'
+	return row[4][:5].lower() == 'noise'
 
 def mp_col(row):
     '''
@@ -42,7 +45,6 @@ def mp_col(row):
         row = row
     return (row[0], row[1], row[2], row[3], row[5], row[49], row[50], row[24], row[25], row[8])
 
-#def mp_timestamp(row):
 def ft_havetime(row):
     return type(row[1]) == datetime.datetime
 
@@ -57,7 +59,10 @@ def mp_groupbykey(row):
     day = time.day
     hour = time.hour
     minute = time.minute
-    return (year, month, day, hour, zipcode), 1
+    #return ((year, month, day, hour, zipcode), 1)
+    return ((year, zipcode), 1)
 
-res = all_comp.filter(ft_header).map(mp_col).filter(ft_noise).filter(ft_havetime)\
-.map(mp_groupbykey).reduceByKey(add)
+res = all_comp.filter(ft_header).map(mp_col)
+ress = res.filter(ft_noise).filter(ft_havetime).map(mp_groupbykey)
+
+print ress.reduceByKey(add).collect()
